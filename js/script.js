9280fc91748e27f2c496bd248abec32d7f3acfab
Kenta Bilotta <kentabilotta20@gmail.com>
2022-11-22 14:47:38 +0100

    modifica che elimina lista precedente

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const app = new Vue({
     },
     methods: {
         generateEmails() {
+            this.arrEmails = [];
             for (i = 0; i < 10; i++) {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((axiosReply) => {
